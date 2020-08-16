8569e7b741cea3e8f81a25176c472cc651d56c7a
Chaha1n <chahaaaaan@gmail.com>
2020-08-16 11:25:24 +0900

    WIP

--- register.py
+++ register.py
@@ -7,16 +7,42 @@ from websocket_server import WebsocketServer
 
 
 class CardRegister(Reader):
+    def read(self, client, server, message):
+        if(message == "client_ready"):
+            clf = nfc.ContactlessFrontend('usb')
+            server.send_message_to_all("listening")  # Listening Tag...
+            print("Touch me")
+            """
+                TODO make GUI
+                like
+                [Listening...]
+                """
+            clf.connect(rdwr={'on-connect': self.on_connect})
+            clf.close()
+        else:
+            server.send("other_error")
+
     def on_connect(self, tag):
         super().on_connect(tag)
-        self.registNewTag()
+        server.send_message_to_all("touch_again")
+        clf.connect(rdwr={'on-connect': self.eliminateRandomTag})
         return True
 
-    def registNewTag(self):
+    def eliminateRandomTag(self, tag):
+        if(tag._nfcid == self.tagIDbeforeConvert):
+            server.send_message_to_all("regist_succeed")
+            self.registNewTag(tag)
+        else:
+            server.send_message_to_all("regist_error")
+        return True
+
+    def registNewTag(self, tag):
         newTagID = self.tagIDbeforeConvert
         # regist new tag
 
 
-while True:
+if __name__ == '__main__':
     register = CardRegister()
-    register.read()
+    server = WebsocketServer(9999, host="localhost")
+    server.set_fn_message_received(register.read)
+    server.run_forever()
