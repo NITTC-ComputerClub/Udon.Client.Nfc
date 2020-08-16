cc30f97872af25f0f64a1bc30034284bf03cf3c0
Chaha1n <chahaaaaan@gmail.com>
2020-08-16 11:17:23 +0900

    client_readyが送出されない問題を修正

--- static/client.js
+++ static/client.js
@@ -1,43 +1,44 @@
-const socket = new WebSocket("ws://localhost:9999/");
-socket.addEventListener('open', function (event) {
-    console.log("System Ready");
-    dif$("#listening").css("display", "block");
-});
-socket.addEventListener('message', function (event) {
-    console.log(event.data);
-    $(".components").css("display", "none");
-    switch (event.data) {
-        case "listening":
-            $("#listening").css("display", "block");
-            break;
-        case "sending":
-            $("#sending").css("display", "block");
-            break;
-        case "record_succeed":
-            $("#succeeded").css("display", "block");
-            drawingOut();
-            break;
-        case "3":
-            $("#connection_error").css("display", "block");
-            drawingOut();
-            break;
-        case "incorrect_tag":
-            $("#tag_error").css("display", "block");
-            drawingOut();
-            break;
-        case "other_error":
-            $("#other_error").css("display", "block");
-            drawingOut();
-            break;
-        default:
-            console.log("Something Wrong...");
-            console.log(event.data);
-            drawingOut();
+window.onload = async () => {
+    const socket = await new WebSocket("ws://localhost:9999/");
+    socket.addEventListener('open', function (event) {
+        console.log("connected");
+        $("#listening").css("display", "block");
+        socket.send("client_ready");
+        console.log("client_ready");
+    });
+    socket.addEventListener('message', function (event) {
+        console.log(event.data);
+        $(".components").css("display", "none");
+        switch (event.data) {
+            case "listening":
+                $("#listening").css("display", "block");
+                break;
+            case "sending":
+                $("#sending").css("display", "block");
+                break;
+            case "record_succeed":
+                $("#succeeded").css("display", "block");
+                drawingOut();
+                break;
+            case "3":
+                $("#connection_error").css("display", "block");
+                drawingOut();
+                break;
+            case "incorrect_tag":
+                $("#tag_error").css("display", "block");
+                drawingOut();
+                break;
+            case "other_error":
+                $("#other_error").css("display", "block");
+                drawingOut();
+                break;
+            default:
+                console.log("Something Wrong...");
+                console.log(event.data);
+                drawingOut();
+        }
+    });
+    function drawingOut() {
+        setTimeout(socket.send("client_ready"), 1500);
     }
-});
-function drawingOut() {
-    setTimeout(socket.send("client_ready"), 1500);
-}
-window.onload = () => {
-    socket.send("client_ready");
 }
