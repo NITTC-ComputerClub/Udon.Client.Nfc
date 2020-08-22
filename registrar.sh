cc350111e7b5af33d20b8aa0e02abfe7d0dc6ae4
Chaha1n <chahaaaaan@gmail.com>
2020-08-22 13:39:36 +0900

    rename from register to registrar

--- registrar.sh
+++ registrar.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ps aux | grep python3 | grep -v grep | awk '{ print "kill -9", $2 }' | sh
-python3 ./scripts/register.py & xdg-open ./clients/register.html
+python3 ./scripts/registrar.py & xdg-open ./clients/registrar.html
