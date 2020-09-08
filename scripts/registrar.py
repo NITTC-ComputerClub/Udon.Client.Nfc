c124380d5d7540e5f9d990d4307c34614cb3e830
Chaha1n <chahaaaaan@gmail.com>
2020-09-08 21:11:50 +0900

    33200f4でmai.pyと同じ内容に誤って変更していたので復元

--- scripts/registrar.py
+++ scripts/registrar.py
@@ -1,95 +1,91 @@
+import json
 import nfc
 import binascii
 import os
-import json
-import requests
-import time
+from main import CardReader
 from websocket_server import WebsocketServer
 
 
-class CardReader:
+class CardRegistrar(CardReader):
     def read(self, client, server, message):
         print(message)
-        if(message == "client_ready"):
-            clf = nfc.ContactlessFrontend('usb')
-            server.send_message_to_all("listening")  # Listening Tag...
-            print("Touch me")
-            clf.connect(rdwr={'on-connect': self.on_connect})
-            clf.close()
+        if(message[:7] == "member:"):
+            self.memberName = message[7:]
+            print(message[7:])
+        elif (message == "client_ready"):
+            pass
         else:
-            server.send("other_error")
+            server.send_message_to_all("other_error")
+            return True
+        clf = nfc.ContactlessFrontend('usb')
+        server.send_message_to_all("listening")  # Listening Tag...
+        print("Touch me")
+        clf.connect(rdwr={'on-connect': self.on_connect})
+        clf.close()
+
+    def __init__(self):
+        self.firstTouch = True
 
     def on_connect(self, tag):
-        tagID = str(binascii.hexlify(tag._nfcid))
-        print("IDm : {IDm}".format(IDm=tagID))
-        self.tagIDbeforeConvert = tagID
-        return True
-
-
-class Recorder(CardReader):
-    def on_connect(self, tag):
-        super().on_connect(tag)
-        if(self.convert_IDm()):
-            # Sending to server...
-            server.send_message_to_all("sending")
-            if(self.recordMemberID()):
-                # Record succeed!
-                server.send_message_to_all("record_succeed")
+        if(self.firstTouch):
+            super().on_connect(tag)
+            self.firstTouch = False
+            server.send_message_to_all("touch_again")
+        else:
+            print(self.tagIDbeforeConvert)
+            print(str(binascii.hexlify(tag._nfcid)))
+            if(self.tagIDbeforeConvert == str(binascii.hexlify(tag._nfcid))):
+                server.send_message_to_all("registering")
+                self.registNewTag()
             else:
-                # Record Error
-                server.send_message_to_all("other_error")
-        else:
-            # This tag does not much any member
-            server.send_message_to_all("incorrect_tag")
-
-        self.memberID = ""
-        self.tagIDbeforeConvert = ""
+                server.send_message_to_all("register_error")
+            self.firstTouch = True
         return True
 
-    def convert_IDm(self):
-        IDbeforeConvert = self.tagIDbeforeConvert
-        f = open("database/members.json", "r")
-        memberData = json.load(f)
-        f.close()
-        for member in memberData:
-            for i in member["IDm"]:
-                if(i == IDbeforeConvert):
-                    self.memberID = member["id"]
-                    print(member["name"])
-                    return True
-        return False  # if it does not much any tag
-    
-    def getToken(self):
-        with open("token.json") as j:
-            lastToken = json.load(j)
-            if(time.time()>lastToken.expirationDate): #expired
-                val = {
-                    "ClientId":"",
-                    "ClientToken":""
-                }
-                r=requests.post("",data=)
-                return
-            else:   #return last token
-                return lastToken.token
-    def recordMemberID(self):
-        UdonURL = ""  # Udon API Server
-        token=self.getToken()
-        
-        """values = {
-            "client": os.environ["Client_ID"],  # TODO set client id
-            "member": self.memberID
-        }
-        data = urllib.parse.urlencode(values)
-        req = urllib.request.Request(UdonURL, data)
-        with urllib.request.urlopen(UdonURL, data=req) as res:
-            print(res.read)  # TODO read Status Code and output something
-            # If error occured,return False
-        """
-        return True
+    def registNewTag(self):
+        with open("database/members.json", "r") as f:
+            memberJson = json.load(f)
+            newTagID = self.tagIDbeforeConvert
+            for element in memberJson:  # check this card does not link any users yet
+                for i in element["IDm"]:
+                    print(i)
+                    if(i == newTagID):
+                        print("this card already linked someone")
+                        server.send_message_to_all("register_error")
+                        return True
+            if(hasattr(self, "memberName") and self.memberName != ""):
+                try:
+                    for i in memberJson:
+                        if(i["name"] == self.memberName):
+                            tmp = i["IDm"]
+                            tmp.append(newTagID)
+                            i["IDm"] = tmp
+                            print(tmp)
+                            break
+                    with open("database/members.json", "w") as fw:
+                        json.dump(memberJson, fw)
+                    server.send_message_to_all("register_succeed")
+                except Exception as e:
+                    print(e)
+                    server.send_message_to_all("register_error")
+
+            else:
+                print("Unknown Error")
+                server.send_message_to_all("register_error")
 
 
-if __name__ == '__main__':
-    recorder = Recorder()
+def sendMemberList(client, server):
+    memberList = "member-list"
+    with open("database/members.json") as f:
+        memberJson = json.load(f)
+        for i in memberJson:
+            memberList += i["name"]+","
+    server.send_message_to_all(memberList)
+
+
+if __name__ == "__main__":
+    registrar = CardRegistrar()
     server = WebsocketServer(9999, host="localhost")
-    server.set_fn_message_received(recorder.read)
+    server.set_fn_new_client(sendMemberList)
+    server.set_fn_message_received(registrar.read)
     server.run_forever()
