5c91499df2491b03fdc2ef096988e6e82730dc3a
Chaha1n <chahaaaaan@gmail.com>
2020-10-23 21:41:17 +0900

    refactor register

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,6 @@
-from udon_modules import udon_api
-from udon_modules import udon_db
 from udon_modules import udon_card
 
 
 if __name__ == "__main__":
     while True:
-        udon_card.begin_reading()
+        udon_card.begin_attendance_reading()
--- scripts/udon_modules/udon_card.py
+++ scripts/udon_modules/udon_card.py
@@ -4,16 +4,32 @@ import os
 import json
 from udon_modules import udon_db
 
-def begin_reading():
+def begin_attendance_reading():
         clf = nfc.ContactlessFrontend("usb")
         print("Listening")
-        clf.connect(rdwr={"on-connect": on_connect})
+        clf.connect(rdwr={"on-connect": on_attendance})
         clf.close()
 
-def on_connect(tag):
+def on_attendance(tag):
     IDm = str(binascii.hexlify(tag._nfcid))
     print("IDm : {IDm}".format(IDm=IDm))
     member_id = udon_db.tomember(IDm)
     #udon_api.record_attendance(member_id)
     print("record succeed")
     return True
+
+def begin_register_reading():
+    clf = nfc.ContactlessFrontend("usb")
+    print("Listening")
+    clf.connect(rdwr={"on-connect": on_register(clf)})
+
+def on_register(tag,clf):
+    tag2 = clf.connect(rdwr={'on-connect': lambda tag2: True})
+    if(tag._nfcid == tag2._nfcid):
+        print("Valid")
+        #udon_db.registrar_newtag(member_id,IDm)
+    else:
+        print("Invalid")
+
+
+
