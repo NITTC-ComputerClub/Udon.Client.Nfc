f714350c13c47030f65664619caf00835ff51c7d
Chaha1n <chahaaaaan@gmail.com>
2020-10-25 14:33:32 +0900

    fixed start shell script

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ps aux | grep python3 | grep -v grep | awk '{ print "kill -9", $2 }' | sh
-/usr/bin/python3 /home/pi/Desktop/Git/Udon.Client.Nfc/scripts/main.py & xdg-open /home/pi/Desktop/Git/Udon.Client.Nfc/clients/main.html;
+/usr/bin/python3 /home/pi/Desktop/Git/Udon.Client.Nfc/clients/main.py;
