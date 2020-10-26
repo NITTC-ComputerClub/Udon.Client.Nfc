8a15d4acf6367f7afe16f590b880c2c345be542a
Chaha1n <chahaaaaan@gmail.com>
2020-10-26 22:23:08 +0900

    changed open()'s option

--- clients/udon_modules/udon_db.py
+++ clients/udon_modules/udon_db.py
@@ -11,20 +11,19 @@ def tomember(IDm):
         return ""  # if does not much any tag
 
 def register_newcard(member_name,new_card):
-    with open("../database/members.json", "r") as f:
-            members_data = json.load(f)
-            
+    try:
+        with open("../database/members.json", "w+") as udon_users_data:
+            members_data = json.load(udon_users_data)
+
             for element in memberJson:  # check this card does not link any users yet
                 for user_card in element["IDm"]:
                     if(user_card == new_card):
                         return "already_used"
 
             members_data[member_name]["IDm"].append(new_card)
-            
-            try:
-                with open("database/members.json", "w") as w:
-                    json.dump(members_data, w)
-                return "register_succeed"
-            except Exception as e:
-                print(e)
-                return "internal_error"
+            json.dump(members_data, udon_users_data)
+            return "register_succeed"
+
+    except Exception as e:
+        print(e)
+        return "internal_error"
